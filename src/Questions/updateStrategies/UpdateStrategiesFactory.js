63c3ed2b32d61507a1540dff4142b2a7378d98c6
Kamil <blueflamesxcode@gmail.com>
2018-04-18 09:22:55 +0200

    Add UpdateStrategiesFactory

---
+++ src/Questions/updateStrategies/UpdateStrategiesFactory.js
@@ -0,0 +1,45 @@
+export const availableStrategies = (function () {
+    return Object.freeze({
+        updateByValue: 'update-by_value',
+        updateRadioBoxState: 'update-radiobox-state'
+    })
+})();
+
+export class UpdateStrategiesFactory {
+    static getStrategyByName(strategyType) {
+        switch (strategyType) {
+            case availableStrategies.updateByValue:
+                return updateByValue;
+
+            case availableStrategies.updateRadioBoxState:
+                return updateByRadioBoxStrategy();
+
+            default:
+                throw "> " + strategyType + " this strategyType is invalid";
+        }
+
+        function updateByValue(model, e) {
+            let target = e.target;
+            model.value = target.value;
+        }
+        
+        function updateByRadioBoxStrategy() {
+            
+        }
+
+        function updateByRadioBoxStrategy() {
+            let lastTarget = undefined;
+
+            return function (model, e) {
+                let target = e.target;
+
+                if (lastTarget === undefined) {
+                    lastTarget = target;
+                } else {
+                    lastTarget.value = 'off';
+                    lastTarget = target;
+                }
+            }
+        }
+    }
+}
